34cdc6d955cb48f0c751722e1a6caaea1c27c2f5
Seiji Emery <seijiemery@gmail.com>
2018-11-03 09:54:06 -0700

    playing around w/ camera

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
+import Camera from 'react-html5-camera-photo';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor (props) {
+    super(props);
+    this.state = {
+      message: ""
+    };
+    this.clicked = this.clicked.bind(this);
+  }
+
+   onTakePhoto (dataUri) {
+    // Do stuff with the dataUri photo...
+    console.log('takePhoto');
+  }
+  clicked () {
+    this.setState({
+      message: "took photo!"
+    });
+  }
+  render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Camera
+          idealFacingMode = "environment"
+          imageType = "png"
+
+          onTakePhoto = { (dataUri) => { this.onTakePhoto(dataUri); } }
+        />
+        <button onClick={this.clicked}>click me!</button>
+        <p>{this.state.message}</p>
       </div>
     );
   }
